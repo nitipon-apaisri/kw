1ef94a463a18d0e9146c376fe1ed6dcb050a533b
nitipon <nitipon.apaisri@gmail.com>
2022-08-27 21:29:15 +0200

    Added infinite scroll for fetch more data for the collection

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,15 @@
-const Card = ({ media }) => {
+const Card = ({ media, mimeType }) => {
     return (
         <div className="w-full aspect-square ">
-            <div className="card-zoom">
-                <div className="card-zoom-image" style={{ backgroundImage: `url(${media})` }}></div>
-            </div>
+            {mimeType === "video/mp4" ? (
+                <div className="card-zoom">
+                    <video src={media} controls={true} />
+                </div>
+            ) : (
+                <div className="card-zoom">
+                    <div className="card-zoom-image" style={{ backgroundImage: `url(${media})` }}></div>
+                </div>
+            )}
             <hr className="w-1/2 mx-auto mt-5 mb-3" style={{ borderColor: "rgb(226 232 240)" }} />
         </div>
     );
